05aca6fc682d560906be2c810affe9c21e68b71e
Wilmer11245 <warnedog@unicartagena.edu.co>
2025-07-08 23:52:00 -0500

    Add files via upload

--- Estudiante.java
+++ Estudiante.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Taller2.ejercicios;
+package Taller1.ejercicios;
 
 /**
  *
@@ -12,20 +12,33 @@ public class Estudiante {
     
     private String nombre;
     private int edad;
+    private String curso;
+
+   
+    public Estudiante() {
+        this.nombre = "Sin nombre";
+        this.edad = 0;
+        this.curso = "Ninguno";
+    }
 
     
     public Estudiante(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
+        this.curso = "Ninguno";
     }
 
-    public Estudiante() {
-        this("Sin nombre", 0); //
+    
+    public Estudiante(String nombre, int edad, String curso) {
+        this(nombre, edad); // llama al constructor de nombre y edad
+        this.curso = curso; // asigna el curso
     }
 
+   
     public void mostrarEstudiante() {
         System.out.println("Nombre: " + this.nombre);
         System.out.println("Edad: " + this.edad);
+        System.out.println("Curso: " + this.curso);
     }
     
 }
