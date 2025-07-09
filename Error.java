876b4cf20e600c1ed75aa939603e314a4d880801
Wilmer11245 <warnedog@unicartagena.edu.co>
2025-07-08 23:30:55 -0500

    Add files via upload

---
+++ Error.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller2.ejercicios;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Error {
+     private int numero;
+
+    public static void esteeselNumero() {
+        // System.out.println(this.numero); 
+    }
+    
+ //error corregido   
+  public void EsteEsElNumero() {
+        System.out.println(this.numero); 
+    }  
+}
