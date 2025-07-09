e747e1076c434706965d8b865ac8b580288b054d
Wilmer11245 <warnedog@unicartagena.edu.co>
2025-07-09 01:33:47 -0500

    Add files via upload

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -2,19 +2,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Taller7.ejercicios;
+package Taller1.ejercicios;
 
 /**
  *
  * @author Usuario
  */
-    public class CuentaBancaria {
+public class CuentaBancaria {
     
     private String numeroCuenta;
     private double saldo;
-    public String tipoCuenta;
+    private String tipoCuenta;
 
-    
+   
+    public CuentaBancaria() {
+        this.numeroCuenta = "000000";
+        this.saldo = 0.0;
+        this.tipoCuenta = "Ahorros";
+    }
+
+   
+    public CuentaBancaria(String numeroCuenta, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.tipoCuenta = tipoCuenta;
+        this.saldo = 0.0; 
+    }
+
+   
     public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
         this.numeroCuenta = numeroCuenta;
         this.saldo = saldo;
@@ -22,24 +36,34 @@ package Taller7.ejercicios;
     }
 
    
+    public void mostrarCuenta() {
+        System.out.println("Número de cuenta: " + this.numeroCuenta);
+        System.out.println("Saldo: $" + this.saldo);
+        System.out.println("Tipo de cuenta: " + this.tipoCuenta);
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
     public double getSaldo() {
         return saldo;
     }
 
     public void setSaldo(double saldo) {
-        if (saldo >= 0) {
-            this.saldo = saldo;
-        } else {
-            System.out.println("El saldo no puede ser negativo.");
-        }
+        this.saldo = saldo;
     }
 
-    
-    public void mostrarDetalles() {
-        System.out.println("Número de cuenta: " + numeroCuenta);
-        System.out.println("Saldo: $" + saldo);
-        System.out.println("Tipo de cuenta: " + tipoCuenta);
+    public String getTipoCuenta() {
+        return tipoCuenta;
     }
+
+    public void setTipoCuenta(String tipoCuenta) {
+        this.tipoCuenta = tipoCuenta;
+    }
+    
 }
-    
-
--- Estudiante.java
+++ Estudiante.java
@@ -2,25 +2,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Taller8.ejercicios;
+package Taller1.ejercicios;
 
 /**
  *
  * @author Usuario
  */
-public class Estudiante extends Persona {
-    String matricula;
+public class Estudiante {
+    
+    private String nombre;
+    private int edad;
+    private String curso;
 
    
-    public Estudiante(String nombre, int edad, String matricula) {
-        super(nombre, edad);
-        this.matricula = matricula;
+    public Estudiante() {
+        this.nombre = "Sin nombre";
+        this.edad = 0;
+        this.curso = "Ninguno";
+    }
+
+    
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.curso = "Ninguno";
+    }
+
+    
+    public Estudiante(String nombre, int edad, String curso) {
+        this(nombre, edad); // llama al constructor de nombre y edad
+        this.curso = curso; // asigna el curso
     }
 
    
-    @Override
-    public void mostrarDetalles() {
-        super.mostrarDetalles();
-        System.out.println("Matrícula: " + matricula);
+    public void mostrarEstudiante() {
+        System.out.println("Nombre: " + this.nombre);
+        System.out.println("Edad: " + this.edad);
+        System.out.println("Curso: " + this.curso);
     }
+    
 }
