b5bcf1514bfbbdbde4458e7bae2576866b5f7e89
Wilmer11245 <warnedog@unicartagena.edu.co>
2025-07-09 01:49:38 -0500

    Add files via upload

--- Empleado.java
+++ Empleado.java
@@ -2,24 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Taller8.ejercicios;
+package Taller7.ejercicios;
 
 /**
  *
  * @author Usuario
  */
-    public class Empleado {
-    String nombre;
-    double salario;
+public class Empleado {
+    
+    public String nombre;
 
-    public Empleado(String nombre, double salario) {
-        this.nombre = nombre;
-        this.salario = salario;
+    private double salario;
+
+    public double getSalario() {
+        return salario;
     }
 
-    public void mostrarDetalles() {
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Salario: $" + salario);
+    public void setSalario(double salario) {
+        if (salario >= 0) {
+            this.salario = salario;
+        } else {
+            System.out.println("El salario no puede ser negativo.");
+        }
     }
 }
-    
