1bd853d1fb97d380e85db8a2ea3c1177ec7eba02
Wilmer11245 <warnedog@unicartagena.edu.co>
2025-07-09 01:24:58 -0500

    Add files via upload

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -2,33 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Taller1.ejercicios;
+package Taller7.ejercicios;
 
 /**
  *
  * @author Usuario
  */
-public class CuentaBancaria {
+    public class CuentaBancaria {
     
     private String numeroCuenta;
     private double saldo;
-    private String tipoCuenta;
+    public String tipoCuenta;
 
-   
-    public CuentaBancaria() {
-        this.numeroCuenta = "000000";
-        this.saldo = 0.0;
-        this.tipoCuenta = "Ahorros";
-    }
-
-   
-    public CuentaBancaria(String numeroCuenta, String tipoCuenta) {
-        this.numeroCuenta = numeroCuenta;
-        this.tipoCuenta = tipoCuenta;
-        this.saldo = 0.0; 
-    }
-
-   
+    
     public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
         this.numeroCuenta = numeroCuenta;
         this.saldo = saldo;
@@ -36,34 +22,24 @@ public class CuentaBancaria {
     }
 
    
-    public void mostrarCuenta() {
-        System.out.println("Número de cuenta: " + this.numeroCuenta);
-        System.out.println("Saldo: $" + this.saldo);
-        System.out.println("Tipo de cuenta: " + this.tipoCuenta);
-    }
-
-    public String getNumeroCuenta() {
-        return numeroCuenta;
-    }
-
-    public void setNumeroCuenta(String numeroCuenta) {
-        this.numeroCuenta = numeroCuenta;
-    }
-
     public double getSaldo() {
         return saldo;
     }
 
     public void setSaldo(double saldo) {
-        this.saldo = saldo;
+        if (saldo >= 0) {
+            this.saldo = saldo;
+        } else {
+            System.out.println("El saldo no puede ser negativo.");
+        }
     }
 
-    public String getTipoCuenta() {
-        return tipoCuenta;
-    }
-
-    public void setTipoCuenta(String tipoCuenta) {
-        this.tipoCuenta = tipoCuenta;
-    }
     
+    public void mostrarDetalles() {
+        System.out.println("Número de cuenta: " + numeroCuenta);
+        System.out.println("Saldo: $" + saldo);
+        System.out.println("Tipo de cuenta: " + tipoCuenta);
+    }
 }
+    
+
---
+++ PruebaUtilidades.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller7.ejercicios;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PruebaUtilidades {
+    public static void main(String[] args) {
+        System.out.println("Suma: " + Utilidades.suma(10, 5));
+        System.out.println("Resta: " + Utilidades.resta(10, 5));
+        System.out.println("Multiplicación: " + Utilidades.multiplicacion(10, 5));
+        System.out.println("División: " + Utilidades.division(10, 5));
+        System.out.println("División entre cero: " + Utilidades.division(10, 0)); 
+    }
+}
