a9b294264916e312783c0ae286414ffcd2466cb2
Wilmer11245 <warnedog@unicartagena.edu.co>
2025-07-09 00:36:33 -0500

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller3.ejercicios;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cliente {
+    private String nombre;
+
+    public static void MostrarNombre() {
+        // System.out.println("Nombre: " + this.nombre); 
+        }
+    public  void mostrarNombre() {
+        System.out.println("Nombre: " + nombre); 
+    }
+}
