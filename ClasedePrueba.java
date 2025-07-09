5c1bb1bedd2e23bef5ee348a93f543af3e30bdee
Wilmer11245 <warnedog@unicartagena.edu.co>
2025-07-08 23:34:19 -0500

    Add files via upload

---
+++ ClasedePrueba.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller3.ejercicios;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ClasedePrueba {
+    public static void main(String[] args) {
+        System.out.println("Suma: " + Matematicas.suma(10, 5));
+        System.out.println("Resta: " + Matematicas.resta(10, 5));
+        System.out.println("Multiplicación: " + Matematicas.multiplicacion(10, 5));
+        System.out.println("División: " + Matematicas.division(10, 5));
+    }
+}
---
+++ Matematicas.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller3.ejercicios;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Matematicas {
+     
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    
+    public static int resta(int a, int b) {
+        return a - b;
+    }
+
+    
+    public static int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    
+    public static double division(int a, int b) {
+        if (b != 0) {
+            return (double) a / b;
+        } else {
+            System.out.println("Error: división por cero");
+            return 0;
+        }
+    }
+}
